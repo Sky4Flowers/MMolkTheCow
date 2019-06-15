93585a445258b6ccfb2d7219398d51f2194099df
ga63nes <hess_maxi@web.de>
2019-06-15 08:14:37 +0200

    Character Movement, Item Rotation/changing and first projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime);
+    }
+
+    public void SetDirection(Vector3 input)
+    {
+        direction = input;
+    }
+
+    public Vector3 GetDirection()
+    {
+        return direction;
+    }
+}
