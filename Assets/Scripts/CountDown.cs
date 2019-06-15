1534347d4e60bf5679b483ed9a8e70729aad18df
Curunir <frostblade97@online.de>
2019-06-15 18:01:22 +0200

    countdown script

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDown : MonoBehaviour
 {
+    public int startTimeInSeconds;
+    public bool isActive;
+    private int currentTime;
+    Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isActive = true;
+        currentTime = startTimeInSeconds;
+        text = gameObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        StartCoroutine("TimerTick");
+    }
+
+    IEnumerator TimerTick()
+    {
+        text.text = ""+currentTime;
+        yield return new WaitForSeconds(1);
+        currentTime--;
+        if(currentTime < 0)
+        {
+            isActive = false;
+            gameObject.SetActive(isActive);
+        }
     }
 }
