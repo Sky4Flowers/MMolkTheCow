1fed5e30f6990d7cb54fb8249d6176c915defbc6
Konstantin Karas <konstantinkaras@onlinehome.de>
2019-06-15 09:03:07 +0200

    highscore setup

---
+++ Assets/Scripts/Score/HighscoreScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreScreen
+{
+    public int winningTeam;
+    public List<PlayerScore> playerScores;
+    public TeamScore[] teamScores;
+
+}
