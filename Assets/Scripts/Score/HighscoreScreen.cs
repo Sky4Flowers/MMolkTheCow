630530e398cd6c0fd40b6559128ede2a551dc3f8
Curunir <frostblade97@online.de>
2019-06-15 13:00:49 +0200

    added highscore

--- Assets/Scripts/Score/HighscoreScreen.cs
+++ Assets/Scripts/Score/HighscoreScreen.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HighscoreScreen
-{
+public class HighscoreScreen : MonoBehaviour
+{ 
     public int winningTeam;
-    public List<PlayerScore> playerScores;
-    public TeamScore[] teamScores;
+    public List<PlayerScore> playerScores = new List<PlayerScore>();
+    public List<TeamScore> teamScores = new List<TeamScore>();
+    private Text winningText;
 
+    private void Start()
+    {
+        winningText = GameObject.Find("WinningTeam").GetComponent<Text>();
+        winningText.text = "TEAM #1 WINS!";
+        winningText.color = Color.yellow;
+        teamScores.Add(new TeamScore(1, 12345, 67890, 7));
+        teamScores.Add(new TeamScore(2, 1345, 6780, 0));
+        playerScores.Add(new PlayerScore(1, 1, 100, 246));
+        playerScores.Add(new PlayerScore(2, 1, 400, 2456));
+        playerScores.Add(new PlayerScore(3, 2, 700, 256));
+        playerScores.Add(new PlayerScore(4, 2, 50, 246));
+    }
 }
--- Assets/Scripts/Score/PlayerScore.cs
+++ Assets/Scripts/Score/PlayerScore.cs
@@ -12,19 +12,38 @@ public class PlayerScore
     private Text playerScore;
     private Text playerScoreValue;
     private string playerTextScore;
-    private PlayerScore(int playerId, int teamId, float health, float longestShot)
+    private Color color = Color.black;
+
+    public PlayerScore(int playerId, int teamId, float health, float longestShot)
     {
         this.teamId = teamId;
         this.playerId = playerId;
         this.longestShot = longestShot;
         this.health = health;
+        string playerScoreId = "Player" + playerId + "Score";
+        string playerScoreValueId = "Player" + playerId + "ScoreValue";
+
+        playerScore = GameObject.Find(playerScoreId).GetComponent<Text>();
+        playerScore.color = color;
+        playerScoreValue = GameObject.Find(playerScoreValueId).GetComponent<Text>();
+        playerScoreValue.color = color;
+
+        if (playerId == 1 || playerId == 3)
+        {
+            color = new Color(0x59 / 255.0f, 0xff / 255.0f, 0x67 / 255.0f);
+        }
+        else
+        {
+            color = new Color(0x04 / 255.0f, 0xf8 / 255.0f, 0xff / 255.0f);
+        }
+        playerScore.color = color;
+        playerScoreValue.color = color;
+        Debug.Log(color.ToString());
         SetScore();
     }
     void SetScore()
     {
-        playerScore = GameObject.Find("Player1Score").GetComponent<Text>();
-        playerTextScore = "PLAYER #" + playerId + "\nHealth:" + "\nFarthest Shot:";
-
-        playerScore.text = playerTextScore;
+        playerScore.text = "PLAYER #" + playerId + "\nHealth:" + "\nFarthest Shot:";
+        playerScoreValue.text = "\n" + health + "\n" + longestShot;
     }
 }
